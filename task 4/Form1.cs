739d9287819c308a7742253ee720ea4075ccdc7e
Arina Hristova <arinahri@ut.ee>
2024-10-17 22:44:31 +0300

    Добавьте файлы проекта.

---
+++ task 4/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            Int64 factorial = 1;
+            for(int i =1; i <= a; i++)
+            {
+                factorial = factorial * i;
+            }
+            label1.Text = $"{factorial}";
+        }
+    }
+}
